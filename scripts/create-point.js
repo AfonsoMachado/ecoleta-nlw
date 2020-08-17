f32fe146f5ff887d9e95ece51156547df60d9ee5
Afonso Machado <afonso777@hotmail.com>
2020-08-16 22:49:33 -0300

    consumindo a api do IBGE com os municipios

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -13,9 +13,33 @@ function populateUFs() {
 
 populateUFs()
 
+function getCities(event) {
+  const citySelect = document.querySelector('select[name=city]')
+  const stateInput = document.querySelector('input[name=state]')
+
+  const ufValue = event.target.value
+
+  // Capturando o nome do estado selecionado
+  const indexOfSelectedState = event.target.selectedIndex
+  stateInput.value = event.target.options[indexOfSelectedState].text
+
+  // console.log(event.target);
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${event.target.value}/municipios`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(cities => {
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      }
+
+      // deixando o campo habilitado
+      citySelect.disabled = false
+    })
+
+}
+
 // capturando o campo de UF
 document
   .querySelector('select[name=uf]')
-  .addEventListener('change', () => {
-    console.log('mudou pvt');
-  })
+  .addEventListener('change', getCities)
