48ebbcd7fdd999b228216c1ca469d4add4913c99
Afonso Machado <afonso777@hotmail.com>
2020-08-22 23:04:35 -0300

    povoando banco

--- src/database/db.js
+++ src/database/db.js
@@ -5,57 +5,71 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database("./src/database/database.sqlite")
 
 // usando o objeto de banco de dados
-db.serialize(() => {
-  // criando um tabela
-  db.run(`
-    CREATE TABLE IF NOT EXISTS places (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      image TEXT,
-      name TEXT,
-      address TEXT,
-      address2 TEXT,
-      state TEXT,
-      city TEXT,
-      items TEXT 
-    );
-  `)
+// db.serialize(() => {
+//   // criando um tabela
+//   db.run(`
+//     CREATE TABLE IF NOT EXISTS places (
+//       id INTEGER PRIMARY KEY AUTOINCREMENT,
+//       image TEXT,
+//       name TEXT,
+//       address TEXT,
+//       address2 TEXT,
+//       state TEXT,
+//       city TEXT,
+//       items TEXT 
+//     );
+//   `)
 
-  const query = `
-    INSERT INTO places (
-      image,
-      name,
-      address,
-      address2,
-      state,
-      city,
-      items
-    ) VALUES (?,?,?,?,?,?,?);
-  `
+//   const query = `
+//     INSERT INTO places (
+//       image,
+//       name,
+//       address,
+//       address2,
+//       state,
+//       city,
+//       items
+//     ) VALUES (?,?,?,?,?,?,?);
+//   `
 
-  const values = [
-    "https://images.unsplash.com/photo-1579756423478-02bc82a97679?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=708&q=80",
-    "Colectoria",
-    "Guilherme Gemballa, Jardim América",
-    "Nº 260",
-    "Santa Catarina",
-    "Rio do Sul",
-    "Resíduos Eletrônicos, Lâmpadas"
-  ]
+//   const values = [
+//     "https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60",
+//     "Papersider",
+//     "Guilherme Gemballa, Jardim América",
+//     "Nº 260",
+//     "Santa Catarina",
+//     "Rio do Sul",
+//     "Resíduos Eletrônicos, Lâmpadas"
+//   ]
 
-  function afterInsertData(err) {
-    if (err) {
-      return console.log(err);
-    }
+//   function afterInsertData(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
 
-    console.log("Cadastrado com sucesso!");
-    console.log(this);
-  }
+//     console.log("Cadastrado com sucesso!");
+//     console.log(this);
+//   }
 
-  // inserindo dados
-  //db.run(query, values, afterInsertData)
+//   // inserindo dados
+//   db.run(query, values, afterInsertData)
 
-  // consultando dados
-  db.all()
+//   // consultando dados
+//   // db.all(`SELECT * FROM places`, function (err, rows) {
+//   //   if (err) {
+//   //     return console.log(err);
+//   //   }
 
-  // deletar
-})
+//   //   console.log("Aqui estão seus registros:")
+//   //   console.log(rows);
+//   // })
+
+//   // deletar
+//   // db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
+//   //   if (err) {
+//   //     return console.log(err);
+//   //   }
+
+//   //   console.log("Registro deletado com sucesso");
+//   // })
+// })
